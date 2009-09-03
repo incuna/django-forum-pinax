0bc0c3e0828f410ee1f87a917a0eb14a3ad3053d
ross@Rossp.org <ross@Rossp.org@8fd21cde-4a2f-0410-9917-19c3686e4e0c>
2009-09-03 11:29:37 +0000

    Issue #42: Make compatible with setuptools.
    Note that you now need to make sure forum/forum is in your PYTHONPATH
    rather than just forum/.
    
    Thanks to Dan Fairs for this suggestion.
    
    
    
    git-svn-id: http://django-forum.googlecode.com/svn/trunk@53 8fd21cde-4a2f-0410-9917-19c3686e4e0c

---
+++ setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+import os
+
+version = 'r52'
+
+LONG_DESCRIPTION = """
+============
+Django Forum
+============
+
+This is a very basic forum application that can plug into any
+existing Django installation and use it's existing templates,
+users, and admin interface.
+"""
+
+setup(
+    name='django-forum',
+    version=version,
+    description="django-forum",
+    long_description=LONG_DESCRIPTION,
+    classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Framework :: Django",
+        "Environment :: Web Environment",
+    ],
+    keywords='forum,django',
+    author='Ross Poulton',
+    author_email='ross@rossp.org',
+    url='http://code.google.com/p/django-forum/',
+    license='BSD',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=['setuptools'],
+)
+
